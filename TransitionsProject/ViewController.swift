1aec5f42fd54a9b652ccdc4ec6e743a4a0cb44c3
thenaz <anaz7963@740s-pl-126.local>
2016-10-03 07:44:58 -0600

    Created TransitionsProject
    10/3

---
+++ TransitionsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionsProject
+//
+//  Created by Nazir, Ammar on 10/3/16.
+//  Copyright © 2016 Nazir, Ammar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
